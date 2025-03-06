f91a4eaa23d0391de9b9493bfb8bb8a2b6171d7d
NeuroCorgi <pokatilov0802@gmail.com>
2025-03-06 13:15:30 +0100

    Test fixed

--- test/loop2.v
+++ test/loop2.v
@@ -9,18 +9,18 @@ module loop(clk,rst, d);
 
    assign d = {r1, r2};
 
-   always@(posedge clk, posedge rst) begin
+   always@(posedge clk or posedge rst) begin
 	  if (rst)
-		 r1 = 1'b0;
+		 r1 <= 1'b0;
 	  else
-		r1 = r2;
+		r1 <= r2;
    end
 
-   always@(posedge clk, posedge rst) begin
+   always@(posedge clk or posedge rst) begin
 	  if (rst)
-		r2 = 1'b1;
+		r2 <= 1'b1;
 	  else
-		r2 = r1;
+		r2 <= r1;
    end
    
 endmodule
