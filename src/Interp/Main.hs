6a4e60e889d15aa934a62e9d775570723c45b1eb
NeuroCorgi <pokatilov0802@gmail.com>
2025-03-17 11:37:20 +0100

    Added simple caching, to not spam json's into the designs directory

--- src/Interp/Main.hs
+++ src/Interp/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Interp.Main
   ( exec
   , readDesign
@@ -14,10 +17,15 @@ import System.Process
 import System.Environment
 
 import Control.Monad (foldM)
+
+import Data.Char (isSpace)
 import Data.List.Extra (trim)
 import Data.List.NonEmpty (NonEmpty, nonEmpty)
 import Data.Map (fromList, empty)
 import Data.String (fromString)
+import Data.Hashable (hash)
+
+import Database.SQLite.Simple
 
 import MyLib
 import Interp.Control
@@ -34,22 +42,42 @@ sanitize ('/' : rest) = '.' : sanitize rest
 sanitize (c : rest) = c : sanitize rest
 sanitize [] = []
 
+lookupCache :: FilePath -> IO (Either FilePath FilePath)
+lookupCache filePath = do
+  file <- withFile filePath ReadMode hGetContents'
+  let h = hash $ filter (not . isSpace) file
+  conn <- open "designs/cache.db"
+  execute_ conn "CREATE TABLE IF NOT EXISTS designs (hash INTEGER PRIMARY KEY, filePath TEXT)"
+  r <- query conn "SELECT filePath FROM designs WHERE hash = ?" [h :: Int]
+  cachePath' <- case r of
+    [[ cachePath :: FilePath ]] -> return (Right cachePath)
+    _ -> do
+      cachePath' <- emptyTempFile "designs" (sanitize filePath ++ ".json")
+      execute conn "INSERT INTO designs (hash, filePath) VALUES (?, ?)" (h :: Int, cachePath' :: String)
+      return (Left cachePath')
+  close conn
+  return cachePath'
+
 readDesign :: FilePath -> IO (NonEmpty Module)
 readDesign filePath = do
-  path <- emptyTempFile "designs" (sanitize filePath ++ ".json")
-  let log = path ++ ".log"
-  -- let path = "design.json"
-  jsonDesign <- do
-    _exitCode <- withFile log WriteMode
-      (\hOut -> do
-          (_, _, _, ph) <- createProcess (proc "yosys" [filePath, "-p", "prep; proc; opt; write_json \"" ++ path ++ "\""])
-                           { std_out = UseHandle hOut
-                           , std_err = UseHandle hOut }
-          return ph)
-      >>= waitForProcess
-    fromString <$> withFile path ReadMode hGetContents'
-  hPutStr stderr "Design json file written to: "
-  hPutStrLn stderr path
+  cachedFilePath <- lookupCache filePath
+  jsonDesign <- case cachedFilePath of
+    Left path -> do
+      let log = path ++ ".log"
+      jsonDesign <- do
+        _exitCode <- withFile log WriteMode
+          (\hOut -> do
+              (_, _, _, ph) <- createProcess (proc "yosys" [filePath, "-p", "prep; proc; opt; write_json \"" ++ path ++ "\""])
+                               { std_out = UseHandle hOut
+                               , std_err = UseHandle hOut }
+              return ph)
+          >>= waitForProcess
+        fromString <$> withFile path ReadMode hGetContents'
+      hPutStr stderr "Design json file written to: "
+      hPutStrLn stderr path
+      return jsonDesign
+    Right path -> do
+      fromString <$> withFile path ReadMode hGetContents'
   modules <- liftEither $ MyLib.mods jsonDesign
   case nonEmpty modules of
     Just modules -> return modules
